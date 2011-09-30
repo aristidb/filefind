3a6fe07f8d987a9727e12ca96a2331889c055bec
Aristid Breitkreuz <aristidb@googlemail.com>
2011-09-30 19:18:05 +0200

    actual recursive traversal

--- Filefind.hs
+++ Filefind.hs
@@ -1,4 +1,32 @@
-module Main where
+module Main (main) where
+
+import Control.Exception
+import Control.Monad
+import System.Environment     (getArgs)
+import System.FilePath
+import System.Posix.Directory
+import System.Posix.Files
+
+recurse :: (FilePath -> IO ()) -> FilePath -> IO ()
+recurse action dir = bracket (openDirStream dir) closeDirStream go
+    where
+      go stream = do
+        name <- readDirStream stream
+        case name of
+          [] -> return ()
+          _ -> do let path = dir </> name
+                  case name of
+                    "." -> return ()
+                    ".." -> return ()
+                    _ -> do action path
+                            stat <- getFileStatus path
+                            when (isDirectory stat) $ recurse action path
+                  go stream
 
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+  args <- getArgs
+  let dir = case args of
+              [] -> "."
+              (x:_) -> x
+  recurse putStrLn dir
